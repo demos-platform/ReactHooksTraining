3228eb8a1369f3e9efabec34349fd2b6fee1bd3c
mujue <mujue@pinduoduo.com>
2020-07-26 10:05:54 +0800

    docs: result

--- src/tasks/task2.js
+++ src/tasks/task2.js
@@ -1,13 +1,15 @@
 import React from "react"
 
-const { useState, useEffect } = React
+const { useState, useEffect, useRef } = React
 
 function Demo() {
   const [count, setCount] = React.useState(0)
+  const countRef = useRef(0)
 
   useEffect(() => {
     const id = setInterval(() => {
-      setCount(count + 1)
+      countRef.current = countRef.current + 1
+      setCount(countRef.current)
     }, 2000)
 
     return () => {
